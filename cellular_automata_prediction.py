5165bb249995a15fc575d32e4dd888f72a2583ae
Hunar Ahmad <39065447+hunar4321@users.noreply.github.com>
2024-08-20 10:50:21 +0300

    Update cellular_automata_prediction.py

--- cellular_automata_prediction.py
+++ cellular_automata_prediction.py
@@ -1,3 +1,9 @@
+'''
+This is a small toy example for educational purposes. 
+Here I'm trying to predict the middle part of Rule 30 of cellular automata using a simple neural network. 
+To Learn more check out the associated YouTube Video: https://youtu.be/TT4I2VIPYrg
+'''
+
 import numpy as np
 import matplotlib.pyplot as plt
 
